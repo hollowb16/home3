9950c6ccf968e115a53a71523374b3fd38127204
MAX <hollowb16@gmail.com>
2021-05-08 01:00:37 +0700

    save

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { SkillComponent } from './skill/skill.component';
+import { ContactMeComponent } from './contact-me/contact-me.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { RouterModule,Routes} from '@angular/router';
+
+const routes: Routes = [
+  {path: "1",component:AboutMeComponent},
+  {path: "2",component:SkillComponent},
+  {path: "3",component:ContactMeComponent},
+  {path: "4",component:PortfolioComponent}
+ 
+  ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutMeComponent,
+    SkillComponent,
+    ContactMeComponent,
+    PortfolioComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
